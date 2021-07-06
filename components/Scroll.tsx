643595ef580eda1cfb1c0c714c53a190ca3a4555
Dylan White <dylan.whitej@gmail.com>
2021-07-06 12:26:52 -0700

    initial commit

---
+++ components/Scroll.tsx
@@ -0,0 +1,12 @@
+export const Scroll: React.FC = ({ text: string }) => {
+  return (
+    <div id="scroll-down">
+  <span class="arrow-down">
+  <!-- css generated icon -->
+  </span>
+  <span id="scroll-title">
+    Scroll down
+    </span>
+</div>
+  )
+}
---
+++ components/Title.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/Title.module.css';
+
+export const Title = () => {
+  return (
+    <section className={styles.titleContainer}>
+      <div className={styles.left}></div>
+      <div className={styles.right}></div>
+      <div className={styles.text}>
+        <h1 className={styles.name}>  
+          <span className={styles.first}>DYLAN</span> 
+          <span className={styles.last}>WHITE</span>
+        </h1>
+        <h5 className={styles.developer}><span className={styles.first}>FULL-STACK</span><span className={styles.last}>DEVELOPER</span></h5>
+      </div>
+    </section>
+  )
+}
